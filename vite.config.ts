87d10ba96e6ce7a9b79ff9455d999b3417949a31
Sriramjj-2007 <jjsriram07@gmail.com>
2026-06-09 20:30:16 +0530

    Add PWA support with vite-plugin-pwa, upgrade Vite and React, update build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,36 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
   server: {
     host: true
   },
-  base: '/espcontrol/'
+  base: '/espcontrol/',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // Automatically updates the app when new changes are deployed
+      // includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-able-icon.png'],
+      manifest: {
+        name: 'ESP Control',
+        short_name: 'ESPControl',
+        description: 'ESP Remote Controller',
+        theme_color: '#1f6feb',
+        background_color: '#0b0f13',
+        display: 'standalone', // Makes it feel like a native mobile/desktop app
+        orientation: 'landscape',
+        start_url: '.',
+        icons: [
+          {
+            src: 'icon.png',
+            sizes: '480x480',
+            type: 'image/png',
+            purpose: 'any maskable' // Required for seamless Android icons
+          }
+        ]
+      }
+    })
+
+  ]
 })
